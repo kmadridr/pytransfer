6aabfd6be7fc4909c86b3f63391e737998384b73
Felipe Guzman <guzpref.96@gmail.com>
2018-05-04 15:19:46 -0700

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -9,30 +9,29 @@ s.listen(35)                     # Now wait for client connection.
 
 print( 'Server listening....')
 
+conn, addr = s.accept()     # Establish connection with client.
+print ('Got connection from', addr)
+
 while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print ('Got connection from', addr)
+	print("Waiting for input")
+	data = conn.recv(1024)
+	print('Server received', repr(data))
+	if data == 'ls':
+		files = glob.glob('files/*')
+		filenames = ''
+		for a in files:
+			filenames += a + '\n'
+			conn.send(filenames)
+	elif data == 'finito':
+		break
+	else:
+		filename=data
+		f = open(filename,'rb')
+		l = f.read(1024)
+		conn.send(l)
 
-    while True:
-        print("Waiting for input")
-        data = conn.recv(1024)
-        print('Server received', repr(data))
-        if data == 'ls':
-            files = glob.glob('files/*')
-            filenames = ''
-            for a in files:
-                filenames += a + '\n'
-            conn.send(filenames)
-        elif data == 'finito':
-            break
-        else:
-            filename=data
-            f = open(filename,'rb')
-            l = f.read(1024)
-            conn.send(l)
+		f.close()
+		print('Done sending')
 
-            f.close()
-            print('Done sending')
-
-    conn.send('Thank you for connecting')
-    conn.close()
+conn.send('Thank you for connecting')
+conn.close()
