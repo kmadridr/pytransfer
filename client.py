5c628a64321efa80a4612ab2dce69286681682ae
Felipe Guzman <guzpref.96@gmail.com>
2018-05-04 11:32:35 -0700

    Finito

--- client.py
+++ client.py
@@ -5,21 +5,22 @@ host = socket.gethostname()     # Get local machine name
 port = 60000                    # Reserve a port for your service.
 
 s.connect((host, port))
-data = input()
-s.send(data)
+while(True):
+	data = raw_input()
+	s.send(data)
 
-if data == 'ls':
-    respond = s.recv(1024)
-    print(respond)
-else:
-
-    f = open(data, 'wb')
-    respond = s.recv(1024)
-    while respond:
-        f.write(s.recv(1024))
-        respond = s.recv(1024)
-
-    f.close()
-    print('Successfully get the file')
-    s.close()
-    print('connection closed')
+	if data == 'ls':
+		respond = s.recv(1024)
+		print(respond)
+	elif data == 'finito':
+		break
+	else:
+		f = open(data, 'w+')
+		respond = s.recv(1024)
+		while (respond):
+			f.write(s.recv(1024))
+			respond = s.recv(1024)
+			f.close()
+			print('Successfully get the file')
+s.close()
+print('connection closed')
